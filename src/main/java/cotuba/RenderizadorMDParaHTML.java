04c4115b575ba346090a91ae14e6d4969917975f
Thiago <thimfont@gmail.com>
2019-08-26 23:46:38 -0300

    refactor: Criando capitulos.

--- src/main/java/cotuba/RenderizadorMDParaHTML.java
+++ src/main/java/cotuba/RenderizadorMDParaHTML.java
@@ -1,10 +1,5 @@
 package cotuba;
 
-import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.layout.element.AreaBreak;
-import com.itextpdf.layout.element.IBlockElement;
-import com.itextpdf.layout.element.IElement;
-import com.itextpdf.layout.property.AreaBreakType;
 import org.commonmark.node.AbstractVisitor;
 import org.commonmark.node.Heading;
 import org.commonmark.node.Node;
@@ -17,17 +12,21 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class RenderizadorMDParaHTML {
-    public void renderiza(Path diretorioDosMD) {
+    public List<Capitulo> renderiza(Path diretorioDosMD) {
+        List<Capitulo> capitulos = new ArrayList<>();
+
         PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
         try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
             arquivosMD
                     .filter(matcher::matches)
                     .sorted()
                     .forEach(arquivoMD -> {
+                        Capitulo capitulo = new Capitulo();
                         Parser parser = Parser.builder().build();
                         Node document = null;
                         try {
@@ -38,7 +37,8 @@ public class RenderizadorMDParaHTML {
                                     if (heading.getLevel() == 1) {
                                         // capítulo
                                         String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-                                        // TODO: usar título do capítulo
+                                        capitulo.setTitulo(tituloDoCapitulo);
+
                                     } else if (heading.getLevel() == 2) {
                                         // seção
                                     } else if (heading.getLevel() == 3) {
@@ -54,6 +54,9 @@ public class RenderizadorMDParaHTML {
                         try {
                             HtmlRenderer renderer = HtmlRenderer.builder().build();
                             String html = renderer.render(document);
+                            capitulo.setConteudoHTML(html);
+
+                            capitulos.add(capitulo);
 
                         } catch (Exception ex) {
                             throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
@@ -65,5 +68,6 @@ public class RenderizadorMDParaHTML {
                     "Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
         }
 
+        return capitulos;
     }
 }
