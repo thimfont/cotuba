88a1f265ee068b2c3b6d3cbd5a0bfb3b098634f8
Thiago <thimfont@gmail.com>
2019-08-26 23:17:30 -0300

    feat: Movendo código gerador do PDF para nova classe.

--- src/main/java/cotuba/Main.java
+++ src/main/java/cotuba/Main.java
@@ -1,13 +1,5 @@
 package cotuba;
 
-import com.itextpdf.html2pdf.HtmlConverter;
-import com.itextpdf.kernel.pdf.PdfDocument;
-import com.itextpdf.kernel.pdf.PdfWriter;
-import com.itextpdf.layout.Document;
-import com.itextpdf.layout.element.AreaBreak;
-import com.itextpdf.layout.element.IBlockElement;
-import com.itextpdf.layout.element.IElement;
-import com.itextpdf.layout.property.AreaBreakType;
 import nl.siegmann.epublib.domain.Book;
 import nl.siegmann.epublib.domain.Resource;
 import nl.siegmann.epublib.epub.EpubWriter;
@@ -24,7 +16,6 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
-import java.util.List;
 import java.util.stream.Stream;
 
 public class Main {
@@ -38,63 +29,8 @@ public class Main {
 
         try {
             if ("pdf".equals(formato)) {
-                try (PdfWriter writer = new PdfWriter(Files.newOutputStream(arquivoDeSaida));
-                     PdfDocument pdf = new PdfDocument(writer);
-                     Document pdfDocument = new Document(pdf)) {
-
-                    PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-                    try (Stream<Path> arquivosMD = Files.list(diretorioDosMD)) {
-                        arquivosMD
-                                .filter(matcher::matches)
-                                .sorted()
-                                .forEach(arquivoMD -> {
-                                    Parser parser = Parser.builder().build();
-                                    Node document = null;
-                                    try {
-                                        document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-                                        document.accept(new AbstractVisitor() {
-                                            @Override
-                                            public void visit(Heading heading) {
-                                                if (heading.getLevel() == 1) {
-                                                    // capítulo
-                                                    String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-                                                    // TODO: usar título do capítulo
-                                                } else if (heading.getLevel() == 2) {
-                                                    // seção
-                                                } else if (heading.getLevel() == 3) {
-                                                    // título
-                                                }
-                                            }
-
-                                        });
-                                    } catch (Exception ex) {
-                                        throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
-                                    }
-
-                                    try {
-                                        HtmlRenderer renderer = HtmlRenderer.builder().build();
-                                        String html = renderer.render(document);
-
-                                        List<IElement> convertToElements = HtmlConverter.convertToElements(html);
-                                        for (IElement element : convertToElements) {
-                                            pdfDocument.add((IBlockElement) element);
-                                        }
-                                        // TODO: não adicionar página depois do último capítulo
-                                        pdfDocument.add(new AreaBreak(AreaBreakType.NEXT_PAGE));
-
-                                    } catch (Exception ex) {
-                                        throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-                                    }
-
-                                });
-                    } catch (IOException ex) {
-                        throw new RuntimeException(
-                                "Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
-                    }
-
-                } catch (Exception ex) {
-                    throw new RuntimeException("Erro ao criar arquivo PDF: " + arquivoDeSaida.toAbsolutePath(), ex);
-                }
+                GeradorPDF geradorPDF = new GeradorPDF();
+                geradorPDF.gera(diretorioDosMD, arquivoDeSaida);
 
             } else if ("epub".equals(formato)) {
                 Book epub = new Book();
